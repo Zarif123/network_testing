4c82fc5728da4b21d91f6090e2ca77079ad9fdc2
Zarif Ceaser <zarifceaser321@gmail.com>
2022-03-08 00:47:54 -0600

    working ipv4 and ipv6

--- scan.py
+++ scan.py
@@ -2,18 +2,33 @@ import sys
 import json
 import time
 import re
+import socket
 import subprocess
 
+# https://stackoverflow.com/a/81899 source for ip address checking
 def get_ipv4(domain):
-    result = subprocess.check_output(["nslookup", domain], \
+    result = subprocess.check_output(["nslookup", "-type=A", domain], \
     timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
-    # https://www.geeksforgeeks.org/extract-ip-address-from-file-using-python/ source of ipv4 regex pattern
-    ipv4_pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
     addresses = []
     for i in result.split():
-        if ipv4_pattern.search(i):
+        try:
+            socket.inet_pton(socket.AF_INET, i)
             addresses.append(i)
-    return addresses[2:]
+        except socket.error:
+            pass
+    return addresses[1:]
+
+def get_ipv6(domain):
+    result = subprocess.check_output(["nslookup", "-type=AAAA", domain], \
+    timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
+    addresses = []
+    for i in result.split():
+        try:
+            socket.inet_pton(socket.AF_INET6, i)
+            addresses.append(i)
+        except socket.error:
+            pass
+    return addresses
 
 def main():
     domains_text = sys.argv[1]
@@ -29,6 +44,7 @@ def main():
     for i in domains:
         domains[i]["scan_time"] = time.mktime(time.localtime())
         domains[i]["ipv4_address"] = get_ipv4(i)
+        domains[i]["ipv6_address"] = get_ipv6(i)
 
     with open(output_json, 'w') as f:
         json.dump(domains, f, sort_keys=True, indent=4)
