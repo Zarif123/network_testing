27b3ffd5181e12d6ee9bd7dd08f893aa70c3e1e5
Zarif Ceaser <zarifceaser321@gmail.com>
2022-03-07 23:39:25 -0600

    scanner framework and working timer

--- scan.py
+++ scan.py
@@ -1,11 +1,27 @@
 import sys, json
-domains_text = sys.argv[1]
-output_json = sys.argv[2]
+import time
+import subprocess
 
-with open(domains_text, 'r') as f:
-    data = f.read()
+#def get_ipv4(domain):
 
-domains = data.split('\n')
 
-with open(output_json, 'w') as f:
-    json.dump(domains, f, sort_keys=True, indent=4)
+def main():
+    domains_text = sys.argv[1]
+    output_json = sys.argv[2]
+
+    with open(domains_text, 'r') as f:
+        data = f.read()
+
+    domains = dict.fromkeys(data.split('\n'))
+    for i in domains:
+        domains[i] = dict()
+
+    for i in domains:
+        domains[i]["scan_time"] = time.mktime(time.localtime())
+        #domains[i]["ipv4_address"] = 
+
+    with open(output_json, 'w') as f:
+        json.dump(domains, f, sort_keys=True, indent=4)
+
+if __name__ == "__main__":
+    main()
