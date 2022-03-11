f90f13dd8c65a6a0e6baf4510b5e530a8174a8e2
Zarif Ceaser <zarifceaser321@gmail.com>
2022-03-10 23:12:50 -0600

    insecure scanner

--- scan.py
+++ scan.py
@@ -50,13 +50,29 @@ def get_geo(ip):
         for i in range(len(ip)):
             #print(len(ip))
             location = reader.get(ip[i])
-            print(location.keys())
+            #print(location.keys())
             city = location['city']['names']['en'] if 'city' in location.keys() else ""
             state = location['subdivisions'][0]['names']['en'] if 'subdivisions' in location.keys() else ""
             country = location['country']['names']['en'] if 'country' in location.keys() else ""
             locations.append(f"{city}, {state}, {country}")
 
     return locations
+
+def get_insecure_http(domain):
+    result = subprocess.check_output(["nmap", domain], \
+        timeout=6, stderr=subprocess.STDOUT).decode("utf-8")
+
+    result = result[result.find("PORT"):]
+    result = result[:result.find("\n\n")]
+    entries = result.split("\n")[1:]
+
+    for entry in entries:
+        port_no = int(entry[:entry.find("/")])
+        if port_no == 80:
+            state = entry.split()[1]
+            return state == "open"
+
+    return False
 
 def main():
     domains_text = sys.argv[1]
@@ -74,6 +90,7 @@ def main():
         domains[i]["ipv4_address"] = get_ipv4(i)
         domains[i]["ipv6_address"] = get_ipv6(i)
         domains[i]["http_server"] = get_server(i)
+        domains[i]["insecure_http"] = get_insecure_http(i)
         domains[i]["geo_locations"] = get_geo(domains[i]["ipv4_address"])
 
     with open(output_json, 'w') as f:
